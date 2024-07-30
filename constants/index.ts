25a98e038672ae8e5e6e43957f87becbebfd1024
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2024-07-30 22:19:40 +0530

    add another memory page.

--- constants/index.ts
+++ constants/index.ts
@@ -4,6 +4,7 @@ export const pageSlugs: { [key: string]: string } = {
     3: "https://arjuncodess.notion.site/18th-March-2023-c2cf6bbfe6e34030a8a74bc725f10705",
     4: "https://arjuncodess.notion.site/22nd-March-2023-7cf90522802a4323b4421bb75a459ce6",
     5: "https://arjuncodess.notion.site/13th-May-2023-dafdcdcee2d94869beb993943f9cff51",
+    6: "https://arjuncodess.notion.site/Letter-To-Mishika-3480ac626a044020b23e91f8ce5f974b",
 };
 
 export const pageKeys = Object.keys(pageSlugs);
