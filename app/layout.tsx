ef2f3653661b6495a829d194bca8c83ff8f69996
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2025-07-30 22:27:34 +0530

    fix(layout): add tracking script to layout component
    - Introduce tracking script for analytics in layout.tsx
    - Maintain existing functionality while enhancing tracking capabilities
    - Ensure proper hydration handling with suppressHydrationWarning attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,27 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { constructMetadata } from "@/lib/utils";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = constructMetadata();
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" suppressHydrationWarning className="overflow-x-hidden">
-            <body className={inter.className} suppressHydrationWarning>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en" suppressHydrationWarning className="overflow-x-hidden">
+      <body className={inter.className} suppressHydrationWarning>
+        {children}
+      </body>
+      <Script
+        defer
+        data-domain="whisperstomishika.vercel.app"
+        src="https://getanalyzr.vercel.app/tracking-script.js"
+      />
+    </html>
+  );
 }
